e4250868068e5935ab72648aa2119ba5a6cf1fe9
FaizanKhanDev <faizan480khan@gmail.com>
2023-09-03 12:29:33 +0500

    setup express js

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 import express from 'express';
-import myLogger from './middleware/logger-middleware.js';
-import web from "./routes/web.js";
-import student from "./routes/student.js"
-
 const app = express()
-const port = process.env.PORT || "3000"
-app.set('views engine', "ejs")
 
 
-app.use('/', web)
-app.use('/', student)
+const port = process.env.PORT || '3000'
 
 
 app.listen(port, () => {
-    console.log(`server listening http://localhost:${port}`)
+    console.log(`Server listenning at http://localhost:${port}`)
 })
